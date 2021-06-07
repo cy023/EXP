3f021491f7f044c4334667276edf2ab3bc71b8b8
cy023 <cyyang@g.ncu.edu.tw>
2021-06-08 02:31:27 +0800

    Feat compare different platform

--- Mem/main.c
+++ Mem/main.c
@@ -1,4 +1,20 @@
-int main()
+int global_uninit_var;
+int global_init0_var = 0;
+int global_init_var = 66;
+
+static int global_static_uninit_var;
+static int global_static_init0_var = 0;
+static int global_static_init_var = 99;
+
+int main(void)
 {
+    int local_uninit_var;
+    int local_init0_var = 0;
+    int local_init_var = 100;
+
+    static int local_static_uninit_var;
+    static int local_static_init0_var = 0;
+    static int local_static_init_var = 77;
+
     return 0;
-} 
+}
