459989901a8b9755e114e7cc3a078af94c207cc4
cy023 <cyyang@g.ncu.edu.tw>
2021-07-28 22:07:49 +0800

    Solve Attack Lab part2-level3

--- CMU15213/Attack_Lab/target1/farm.c
+++ CMU15213/Attack_Lab/target1/farm.c
@@ -4,41 +4,53 @@ int start_farm()
     return 1;
 }
 
+// ?
 unsigned getval_142()
 {
     return 2425387259U;
 }
 
+// 48 89 c7 c3
+// movq     %rax, %rdi
 unsigned addval_273(unsigned x)
 {
     return x + 3284633928U;
 }
 
+// 58 90 c3
+// popq     %rax
 unsigned addval_219(unsigned x)
 {
     return x + 2421715793U;
 }
 
+// ?
 void setval_237(unsigned *p)
 {
     *p = 3351742792U;
 }
 
+// ?
 void setval_424(unsigned *p)
 {
     *p = 2455290452U;
 }
 
+// ?
 void setval_470(unsigned *p)
 {
     *p = 3347925091U;
 }
 
+// 48 89 c7 90 c3
+// movq     %rax, %rdi
 void setval_426(unsigned *p)
 {
     *p = 2428995912U;
 }
 
+// 58 90 c3
+// popq     %rax
 unsigned getval_280()
 {
     return 3281016873U;
@@ -56,161 +68,213 @@ long add_xy(long x, long y)
     return x+y;
 }
 
+// 89 c2 90 c3
+// movl     %eax, %edx
 unsigned getval_481()
 {
     return 2428668252U;
 }
 
+// ?
 void setval_296(unsigned *p)
 {
     *p = 2425409945U;
 }
 
+// ?
 unsigned addval_113(unsigned x)
 {
     return x + 3380137609U;
 }
 
+// 20 db c3
+// andb     %bl, %bl
 unsigned addval_490(unsigned x)
 {
     return x + 3676361101U;
 }
 
+// ?
 unsigned getval_226()
 {
     return 3225997705U;
 }
 
+// 84 c0 c3
+// testb        %al, %al
 void setval_384(unsigned *p)
 {
     *p = 3229929857U;
 }
 
+// 48 89 e0 c3
+// mov  %rsp, %rax
 unsigned addval_190(unsigned x)
 {
     return x + 3767093313U;
 }
 
+// 08 c9 c3
+// orb      %cl, %cl
 void setval_276(unsigned *p)
 {
     *p = 3372794504U;
 }
 
+// 89 ce 90 90 c3
+// movl     %ecx, %esi
 unsigned addval_436(unsigned x)
 {
     return x + 2425409161U;
 }
 
+// ?
 unsigned getval_345()
 {
     return 3252717896U;
 }
 
+// ?
 unsigned addval_479(unsigned x)
 {
     return x + 3372270217U;
 }
 
+// 38 c0 c3
+// cmpb    %al, %al
 unsigned addval_187(unsigned x)
 {
     return x + 3224948361U;
 }
 
+// 08 db c3
+// orb      %bl, %bl
 void setval_248(unsigned *p)
 {
     *p = 3674787457U;
 }
 
+// 89 d1 38 c9 c3
+// movl     %edx, %ecx
+// cmpb     %cl, %cl
 unsigned getval_159()
 {
     return 3375944073U;
 }
 
+// 89 e0 c3
+// movl     %esp, %eax
 unsigned addval_110(unsigned x)
 {
     return x + 3286272456U;
 }
 
+// 84 c0 c4
+// testb        %al, %al
 unsigned addval_487(unsigned x)
 {
     return x + 3229926025U;
 }
 
+// ?
 unsigned addval_201(unsigned x)
 {
     return x + 3353381192U;
 }
 
+// 08 d2 c3
+// orb      %dl, %dl
 unsigned getval_272()
 {
     return 3523793305U;
 }
 
+// ?
 unsigned getval_155()
 {
     return 3385115273U;
 }
 
+// ?
 void setval_299(unsigned *p)
 {
     *p = 2447411528U;
 }
 
+// ?
 unsigned addval_404(unsigned x)
 {
     return x + 3281178249U;
 }
 
+// 89 d1 08 db c3
+// movl     %edx, %ecx
+// orb      %bl ,%bl
 unsigned getval_311()
 {
     return 3674788233U;
 }
 
+// ?
 void setval_167(unsigned *p)
 {
     *p = 3281113481U;
 }
 
+// 38 d2 c3
+// cmpb     %dl, %dl
 void setval_328(unsigned *p)
 {
     *p = 3526935169U;
 }
 
+// 08 c9 c3
+// orb      %cl, %cl
 void setval_450(unsigned *p)
 {
     *p = 3372797449U;
 }
 
+// 89 e0 90 c3
+// movl     %esp, %eax
 unsigned addval_358(unsigned x)
 {
     return x + 2430634248U;
 }
 
+// ?
 unsigned addval_124(unsigned x)
 {
     return x + 1019724425U;
 }
 
+// 20 c0 c3
+// andb     %al, %al
 unsigned getval_169()
 {
     return 3223375496U;
 }
 
+// ?
 void setval_181(unsigned *p)
 {
     *p = 3269495112U;
 }
 
+// ?
 unsigned addval_184(unsigned x)
 {
     return x + 3529556617U;
 }
 
+// 20 d2 c3
+// andb     %al, %al
 unsigned getval_472()
 {
     return 3525365389U;
 }
 
+// ?
 void setval_350(unsigned *p)
 {
     *p = 2430634312U;
